35188392e5cdade76eb05389277a5ca2b98368c1
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2022-07-02 23:28:55 -0500

    move components to bridge page

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 
-import response from './constants/response';
-
-import { DataContext } from './context/DataContext';
-
 import Navbar from './components/Navbar';
-import BridgeContainer from './components/BridgeContainer';
+import Bridge from './pages/Bridge';
 
 function App() {
-
-    const { from, setFrom, to, setTo, token, setToken, amount, setAmount, expectedAmount, setExpectedAmount, data, setData, fees, setFees, protocol, setProtocol, contractInfo, setContractInfo, networks, setNetworks } = useContext(DataContext);
-
-    useEffect(() => {
-        const data = response;
-        setData(data);
-    }
-    , []);
-
-    useEffect(() => {
-        if (protocol === '') {
-            setExpectedAmount(amount);
-        } else { 
-            setExpectedAmount(amount * 0.99);
-            setFees(amount * 0.01);
-        }
-    }
-    , [amount, protocol]);
         
     return (
         <div className="App">
             <Navbar />
-            <BridgeContainer />
+            <Bridge />
         </div>
     );
 }
--- src/pages/Bridge.jsx
+++ src/pages/Bridge.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+
 import BridgeContainer from "../components/BridgeContainer";
+import Title from "../components/Title";
+
+import { DataContext } from "../context/DataContext";
+
+import response from "../constants/response";
 
 export default function Bridge() {
+
+    const { from, setFrom, to, setTo, token, setToken, amount, setAmount, expectedAmount, setExpectedAmount, data, setData, fees, setFees, protocol, setProtocol, contractInfo, setContractInfo, networks, setNetworks } = useContext(DataContext);
+
+    useEffect(() => {
+        const data = response;
+        setData(data);
+    }
+    , []);
+
+    useEffect(() => {
+        if (protocol === '') {
+            setExpectedAmount(amount);
+            setFees(0);
+        } else { 
+            setExpectedAmount(amount * 0.99);
+            setFees(amount * 0.01);
+        }
+    }
+    , [amount, protocol]);
+
     return (
         <div className="bridge">
             <Title />
