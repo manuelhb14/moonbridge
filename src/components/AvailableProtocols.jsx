5533cb850aa7dfd831538d3bb2539016b5ee519b
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2022-07-10 11:26:41 -0500

    add styling to available protocols

--- src/components/AvailableProtocols.jsx
+++ src/components/AvailableProtocols.jsx
@@ -1,29 +1,63 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useState } from "react";
+import { useEffect } from "react";
+import Select from "react-select";
 
 import { DataContext } from "../context/DataContext";
+
+import multichainLogo from "../logos/multichain.svg";
+import synapseLogo from "../logos/synapse.svg";
+import noneLogo from "../logos/none.svg";
 
 export default function AvailableProtocols() {
     const { protocol, setProtocol, data, from, to, token } = useContext(DataContext);
 
+    const [protocols, setProtocols] = useState([]);
 
+    useEffect(() => {
+        if (from !== process.env.REACT_APP_MOONBEAM_CHAIN_ID) {
+            setProtocols(data.filter((item) => item.srcChainID === from && item.SrcToken.Symbol === token)
+                .map((item) => {
+                    return {
+                        value: item.bridge,
+                        label: item.bridge,
+                        image: item.bridge === "Multichain" ? multichainLogo : synapseLogo
+                    }
+                }
+                ));
+        } else {
+            setProtocols(data.filter((item) => item.srcChainID === to && item.DestToken.Symbol === token)
+                .map((item) => {
+                    return {
+                        value: item.bridge,
+                        label: item.bridge,
+                        image: item.bridge === "Multichain" ? multichainLogo : synapseLogo
+                    }
+                }
+                ));
+        }
+    }
+    , [data, from, token]);
+
+    const onChange = (e) => {
+        setProtocol(e.value);
+    }
+
+    const customStyles = {
+    }
+    
     return (
-        <select name="protocol" id="protocol" value={protocol} onChange={(e) => setProtocol(e.target.value)}>
-            <option value="">Protocol</option>
-            {from !== process.env.REACT_APP_MOONBEAM_CHAIN_ID ?
-            (
-                data.filter((item) => item.srcChainID === from && item.SrcToken.Symbol === token)
-                    .map((item) => {
-                        return <option key={item.bridge} value={item.bridge}>{item.bridge} </option>
-                    }
-                    )
-            ) : (
-                data.filter((item) => item.srcChainID === to && item.DestToken.Symbol === token)
-                    .map((item) => {
-                        return <option key={item.bridge} value={item.bridge}>{item.bridge} </option>
-                    }
-                    )
-            )
-            }
-        </select>
+        <Select
+            options={protocols}
+            onChange={onChange}
+            value={ protocols.find(item => item.value === protocol) ? protocols.find(item => item.value === protocol) : null }
+            placeholder="Protocol"
+            formatOptionLabel={(option) => (
+                <div className="select-option">
+                    <img src={option.image} height="25px" width="25px" alt="logo" />
+                    <span>{option.label}</span>
+                </div>
+            )}
+            styles={customStyles}
+        />
     )
 }
