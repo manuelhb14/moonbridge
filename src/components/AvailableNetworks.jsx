8d757c530926b8006ab7bbc440a23fc4b28206b1
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2022-07-10 10:05:39 -0500

    add styling to select menus TODO add custom styles

--- src/components/AvailableNetworks.jsx
+++ src/components/AvailableNetworks.jsx
@@ -1,20 +1,27 @@
 import React, { useContext } from 'react';
+import Select from 'react-select';
 import { ethers } from "ethers";
-
 import { DataContext } from '../context/DataContext';
+
+import ethLogo from '../logos/eth.svg';
+import avalancheLogo from '../logos/avalanche.svg';
+import moonbeamLogo from '../logos/moonbeam.png';
+import optimismLogo from '../logos/optimism.svg';
+import polygonLogo from '../logos/polygon.svg';
+import bscLogo from '../logos/bsc.svg';
 
 export default function AvailableNetworks({value}) {
 
-    const { isConnected, networks, to, setTo, from, setFrom, setToken, setAmount, setFees, setProtocol, setTokenInfo, setIsApproved } = useContext(DataContext);
+    const { isConnected, to, setTo, from, setFrom, setToken, setAmount, setFees, setProtocol, setTokenInfo, setIsApproved } = useContext(DataContext);
 
     const moonbeamChainID = process.env.REACT_APP_MOONBEAM_CHAIN_ID;
 
     const onFromChange = async (e) => {
         if (isConnected) {
             const oldFrom = from;
-            setFrom(e.target.value);
-            console.log(e.target.value);
-            const hexId = ethers.utils.hexValue(ethers.utils.hexlify(parseInt(e.target.value)));
+            setFrom(e.value);
+            console.log(e.value);
+            const hexId = ethers.utils.hexValue(ethers.utils.hexlify(parseInt(e.value)));
             console.log(hexId);
             await window.ethereum.request({
                 method: 'wallet_switchEthereumChain',
@@ -25,7 +32,7 @@ export default function AvailableNetworks({value}) {
             }
             );
         } else {
-            setFrom(e.target.value);
+            setFrom(e.value);
         }
         setToken('');
         setAmount('');
@@ -37,7 +44,7 @@ export default function AvailableNetworks({value}) {
 
     const onToChange = async (e) => {
         if (isConnected) {
-            setTo(e.target.value);
+            setTo(e.value);
         }
         setToken('');
         setAmount('');
@@ -47,30 +54,55 @@ export default function AvailableNetworks({value}) {
         setIsApproved(false);
     }
 
+    const options = [
+        { value: '1', label: "Ethereum", image: ethLogo },
+        { value: '10', label: "Optimism", image: optimismLogo },
+        { value: '25', label: "Cronos", image: ethLogo },
+        { value: '56', label: "Binance Smart Chain", image: bscLogo },
+        { value: '137', label: "Polygon", image: polygonLogo },
+        { value: '250', label: "Fantom", image: ethLogo },
+        { value: '288', label: "Boba", image: ethLogo },
+        { value: '1088', label: "Metis", image: ethLogo },
+        { value: '1285', label: "Moonriver", image: ethLogo },
+        { value: '42161', label: "Arbitrum", image: ethLogo },
+        { value: '43114', label: "Avalanche", image: avalancheLogo }
+    ];
+
+    const option = [
+        { value: '1284', label: "Moonbeam", image: moonbeamLogo }
+    ]
+
+    const customStyles = {
+    }
+
     return (
         <>
         { ( value === "from") ?
-            <select name="from" id="from" onChange={onFromChange} value={from}>
-                {Object.keys(networks).map((key) => {
-                    if (from !== moonbeamChainID && key !== moonbeamChainID) {
-                        return <option key={key} value={key}>{networks[key]}</option>
-                    } else if (from === moonbeamChainID && key === moonbeamChainID) {
-                        return <option key={key} value={key}>{networks[key]}</option>
-                    }
-                }
+            <Select
+                options={ from === moonbeamChainID ? option : options }
+                onChange={onFromChange}
+                value={ from === moonbeamChainID ? option.find(option => option.value === from) : options.find(option => option.value === from) }
+                formatOptionLabel={(option) => (
+                    <div className="select-option">
+                        <img src={option.image} height="25px" width="25px" alt="logo" />
+                        <span>{option.label}</span>
+                    </div>
                 )}
-            </select>
+                styles={customStyles}
+            />
             :
-            <select name="to" id="to" onChange={onToChange} value={to}>
-                {Object.keys(networks).map((key) => {
-                    if (to !== moonbeamChainID && key !== moonbeamChainID) {
-                        return <option key={key} value={key}>{networks[key]}</option>
-                    } else if (to === moonbeamChainID && key === moonbeamChainID) {
-                        return <option key={key} value={key}>{networks[key]}</option>
-                    }
-                }
+            <Select
+                options={ to === moonbeamChainID ? option : options }
+                onChange={onToChange}
+                value={ to === moonbeamChainID ? option.find(option => option.value === to) : options.find(option => option.value === to) }
+                formatOptionLabel={(option) => (
+                    <div className="select-option">
+                        <img src={option.image} height="25px" width="25px" alt="logo" />
+                        <span>{option.label}</span>
+                    </div>
                 )}
-            </select>
+                styles={customStyles}
+            />
         }
         </>
     )
