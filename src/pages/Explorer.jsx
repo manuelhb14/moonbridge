de127038bd6c05bdf1d348ff2127e368796cbbeb
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2022-07-10 23:59:05 -0500

    add info from endpoints

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { DataContext } from "../context/DataContext";
 
@@ -11,6 +11,8 @@ import SearchBar from "../components/Searchbar";
 export default function Explorer() {
 
     const { txInfo, setTxInfo } = useContext(DataContext);
+    const [price, setPrice] = useState(0);
+    const [stats, setStats] = useState([]);
 
     useEffect(() => {
         fetch("https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-41813a12-6cd0-49c0-abb9-68838a3e1f30/default/explorerTxns")
@@ -23,7 +25,21 @@ export default function Explorer() {
                 console.log(data);
                 setTxInfo(data);
             }
-            )
+        )
+        fetch("https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-41813a12-6cd0-49c0-abb9-68838a3e1f30/default/stats")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setStats(data);
+            }
+        )
+        fetch("https://api.kraken.com/0/public/Ticker?pair=GLMRUSD")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setPrice(data.result.GLMRUSD.c[0]);
+            }
+        )
     }
     , []);
 
@@ -41,18 +57,18 @@ export default function Explorer() {
             <div class="col-6 col-sm-3 card">
             <div class="row align-items-center">
                 <div class="col-2 col-sm-2 "><img className="logo" src="assets/imgs/logos/moonbeam.png" ></img></div>
-                <div class="col-10 col-sm-10 card-content"><span className="ts-sec"> GLMR PRICE </span><br/> <span className="ts-subtitle"> $ 0.63</span></div>
+                <div class="col-10 col-sm-10 card-content"><span className="ts-sec"> GLMR PRICE </span><br/> <span className="ts-subtitle">{price}</span></div>
                 </div>
             </div>
             <div class="col-6 col-sm-3 card">
-                <div class="card-content"><span className="ts-sec"> Bridge Transactions </span><br/> <span className="ts-subtitle"> 2997</span><span className="spancard ts-main"> /24hr</span></div>
+                <div class="card-content"><span className="ts-sec"> Bridge Transactions </span><br/> <span className="ts-subtitle"> {stats.count}</span><span className="spancard ts-main"> / 365 days</span></div>
             </div>
          
             <div class="col-6 col-sm-3 card">
-            <div class="card-content"><span className="ts-sec"> Volume </span><br/> <span className="ts-subtitle"> $43M</span><span className="spancard ts-main"> /24hr</span></div>
+            <div class="card-content"><span className="ts-sec"> Volume </span><br/> <span className="ts-subtitle">{(stats.total / 1000000).toFixed(2)}</span><span className="spancard ts-main"> / 365 days</span></div>
             </div>
             <div class="col-6 col-sm-3 card">
-            <div class="card-content"><span className="ts-sec"> Av. Block Time </span><br/> <span className="ts-subtitle"> 1.8</span><span className="spancard ts-main"> minutes</span></div>
+            <div class="card-content"><span className="ts-sec"> Av. Bridge Time </span><br/> <span className="ts-subtitle">{ stats.waitTime }</span><span className="spancard ts-main"> min</span></div>
             </div>
             </div>
             </div>
